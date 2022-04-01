635a90995853842dac14d3e5f79a77969ab31351
charlesnnanna <charlesnnanna1667@gmail.com>
2022-04-01 07:00:02 -0700

    Uploaded files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './components/card.component';
+import { FormComponent } from './components/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    FormComponent   
   ],
   imports: [
     BrowserModule
